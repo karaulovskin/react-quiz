e07b3cdcbd2f6c24fdae30293b1f8b6d9d01f690
Karaulovskin <karaulovskin@yandex.ru>
2020-04-19 22:23:10 +0300

    Список вопросов

---
+++ src/components/ActiveQuiz/AnswerList/AnswerList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./AnswerList.module.css"
+import AnswerItem from "./AnswerItem/AnswerItem"
+
+const AnswerList = props => (
+    <ul className={classes.AnswerList}>
+        {props.answers.map((answer, index) => {
+            return (
+                <AnswerItem
+                    key={index}
+                    answer={answer}
+                />
+            )
+        })}
+    </ul>
+)
+
+export default AnswerList
